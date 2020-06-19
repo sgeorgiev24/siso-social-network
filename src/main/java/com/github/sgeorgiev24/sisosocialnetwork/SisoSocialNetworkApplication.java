7b85e3d4bebf472d2c51bc84a5dc0aa3d8dc1eee
Stanislav Georgiev <sgeorgiev24@gmail.com>
2020-06-19 16:57:13 +0300

    Initial commit with security and application.properties configuration.
    Signed-off-by: Stanislav Georgiev <sgeorgiev24@gmail.com>

---
+++ src/main/java/com/github/sgeorgiev24/sisosocialnetwork/SisoSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.github.sgeorgiev24.sisosocialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisoSocialNetworkApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SisoSocialNetworkApplication.class, args);
+  }
+
+}
